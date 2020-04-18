34fcdeb2520b4882c269ecf92f78f04efbd79b83
Tetiana <t.pasichnyk@ukr.net>
2020-04-19 01:04:43 +0300

    done

--- Confirm.js
+++ Confirm.js
@@ -1,62 +1,22 @@
 function confirmEnding(str, target) {
 
-    var strSplit = str.split('');
-    var targetSplit = target.split('');
+
     var newstr=''
     for (i=str.length-target.length; i<str.length; i+=1){
-        // var newstr=str[i];
         newstr+=str[i]
-        var newtarger=target[0]; // WTF?
-        // if(newtarger == newstr) {
-        //
-        // }
-
-        // for (m=0; m<target.length; m+=1){
-        //
-        //     if (str[i] == target[m]){
-        //         target[m]=target[m+1];
-        //         console.log(target[m+1]);
-        //     }else {
-        //         // return str;
-        //     }
-        // }
-
+        // var newtarger=target[0];
     }
-    if(newstr == target){
-        console.log('WIN')
-    }else {
-        console.log('POTRACHENO')
-    }
-    // for (i=str.length; i>str.length-target.length-1; i-=1){
-    //     var targetnew=target.length;
-    //     for (m=target.length; m>0-1; m-=1){
-    //         if(str[i] == targetnew){
-    //             targetnew=target[m-1]
-    //         }
-    //         console.log(target[m]);
-    //     }
-        //     if (str[i] == target[m]){
-        //         target[m]=target[m+1];
 
-        //     }else {
-        //         // return str;
-        //     }
-        // }
-
-
-    // }
-
-
-
-    // var targetSplit = target.split('');
-    // for (i=strSplit.length-target.length; i>)
-    // if(strSplit[strSplit.length-1] == target){
-    //     alert( "da!" );
+    // if(newstr == target){
+    //     console.log(newstr === target);
+    //
+    //
     // }else {
-    //     alert( "net!" );
+    //     console.log(newstr == target); //тут ок
     // }
-
-    return str;
+    // console.log(newstr == target);
+    return newstr == target;
 }
 
-confirmEnding("He has to give me a new name", "name");
+// confirmEnding("He has to give me a new name", "name");
+console.log(confirmEnding("He has to give me a new name", "name"))
